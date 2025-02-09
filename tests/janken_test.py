c23245049ed8fcaf247e713999d7599ca090eb0d
Sato Shunta <s.sato.sys24@morijyobi.ac.jp>
2025-02-10 03:05:08 +0900

    janken.pyのテスト

--- tests/janken_test.py
+++ tests/janken_test.py
@@ -1,9 +1,12 @@
 import sys
 import os
+
 sys.path.append(os.path.abspath('../source'))
+
 import unittest
 from unittest.mock import patch
-from janken import janken_main
+from source.janken import janken_main
+
 
 class TestJanken(unittest.TestCase):
 
@@ -11,16 +14,13 @@ class TestJanken(unittest.TestCase):
     @patch('computer.pon')
     @patch('janken_judge.judge')
     def test_janken_main(self, mock_judge, mock_cpu_pon, mock_human_pon):
-        # モックの設定: プレイヤーとコンピュータの手を固定
-        mock_human_pon.return_value = 'グー'  # ユーザーの手
-        mock_cpu_pon.return_value = 'チョキ'  # コンピュータの手
-        mock_judge.return_value = 'あなたの勝ち'  # 勝者を固定
+        mock_human_pon.return_value = 'グー'
+        mock_cpu_pon.return_value = 'チョキ'
+        mock_judge.return_value = 'あなたの勝ち'
 
-        # janken_mainのテスト
         with patch('builtins.print') as mock_print:
-            janken_main()  # ゲーム実行
+            janken_main()
 
-            # printの呼び出しを確認して、結果が期待通りかチェック
             mock_print.assert_any_call('あなたの手: グー, コンピュータの手: チョキ')
             mock_print.assert_any_call('結果: あなたの勝ち')
             mock_print.assert_any_call('最終結果:')
@@ -32,16 +32,13 @@ class TestJanken(unittest.TestCase):
     @patch('computer.pon')
     @patch('janken_judge.judge')
     def test_janken_main_draw(self, mock_judge, mock_cpu_pon, mock_human_pon):
-        # モックの設定: プレイヤーとコンピュータの手を引き分けに設定
-        mock_human_pon.return_value = 'グー'  # ユーザーの手
-        mock_cpu_pon.return_value = 'グー'  # コンピュータの手
-        mock_judge.return_value = '引き分け'  # 勝者を引き分けに設定
+        mock_human_pon.return_value = 'グー'
+        mock_cpu_pon.return_value = 'グー'
+        mock_judge.return_value = '引き分け'
 
-        # janken_mainのテスト（引き分け結果）
         with patch('builtins.print') as mock_print:
-            janken_main()  # ゲーム実行
+            janken_main()
 
-            # printの呼び出しを確認して、結果が期待通りかチェック
             mock_print.assert_any_call('あなたの手: グー, コンピュータの手: グー')
             mock_print.assert_any_call('結果: 引き分け')
             mock_print.assert_any_call('最終結果:')
